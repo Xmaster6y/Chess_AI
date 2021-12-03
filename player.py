f32aa92595ac7a289829319024fa587f86224aa0
Xmaster6y <yoann.poupart@ens-lyon.fr>
2021-12-03 18:12:51 +0100

    First commit

---
+++ player.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Class for the player.
+"""
+import numpy as np
+
+import rules
+
+class Player():
+
+	__init__(self, is_AI, color):
+		self.is_AI = is_AI
+		self.color = color
+
+	def choose_move(self, board_state, policy):
+		return policy(self, board_state)
+
+def random_policy(player, board):
+	moves = rules.possible_moves(board, player.color)
+	return np.random.choice(moves)
+
+def human_policy(player, board):
+	if player.is_AI:
+		return random_policy(player, board)
+	while True:
+		try:
+			print(board)
+			move_str = input("Give your move (exp:e4 e5)\n").split()
+			move = (
+					(int(move_str[0][1])-1, ord(move_str[0][0])-97), #The column index is the letter
+					(int(move_str[1][1])-1, ord(move_str[1][0])-97),
+				)
+			assert rules.is_valid_move(player, board, move)
+		except:
+			print("Invalid move or format!")
+
+	return np.random.choice(moves)
+
+if __name__ == '__main__':
+	pass
---
+++ rules.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Implementation of the rules.
+"""
+
+def possible_moves(board, color):
+	pass
+
+#Beware when calling the following functions, no test is run in these
+def _paw_possible_move(board, color, pos):
+	pass
+
+def _bishop_possible_move(board, color, pos):
+	pass
+
+def _rook_possible_move(board, color, pos):
+	pass
+
+def _king_possible_move(board, color, pos):
+	pass
+
+def _queen_possible_move(board, color, pos):
+	pass
+
+def _knight_possible_move(board, color, pos):
+	pass
+
+
+
+def is_valid_move(player, board, move):
+	pass
+
+#Beware when calling the following functions, no test is run in these
+def _paw_valid_move(board, color, move):
+	pass
+
+def _bishop_valid_move(board, color, move):
+	pass
+
+def _rook_valid_move(board, color, move):
+	pass
+
+def _king_valid_move(board, color, move):
+	pass
+
+def _queen_valid_move(board, color, move):
+	pass
+	
+def _knight_valid_move(board, color, move):
+	pass
+
+if __name__ == '__main__':
+	pass
